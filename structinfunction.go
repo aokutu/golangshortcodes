3fac98c370e351184a28957ab687e5094a09ad9b
aokutu <andreaokutu@gmail.com>
2026-01-29 15:24:22 +0300

    advanced structs in functions

--- structinfunction.go
+++ structinfunction.go
@@ -1,5 +1,3 @@
-// You can edit this code!
-// Click here and start typing.
 package main
 
 import "fmt"
@@ -9,16 +7,31 @@ type rectangle struct {
 	width  int
 }
 
+type Triangle struct {
+	height int
+	width  int 
+
+}
+
 func main() {
 
 	var r1 rectangle
 	r1.length = 5
 	r1.width = 4
-	fmt.Println(Areax(r1))
+	var r2 Triangle
+	r2.width = 4
+	r2.height = 5
+	fmt.Println("RECT",Areax(r1))
+
+	fmt.Println("TRI",Trireax(r2))
 
 }
 
-func Areax(rec rectangle) int {
 
+func Areax(rec rectangle) int {
 	return rec.width * rec.length
+}
+
+func Trireax(tri Triangle) int {
+	return (tri.width * tri.height) / 2
 }
