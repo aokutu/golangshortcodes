2386cabf14e0a28d9aa71c16eb3186d0c73f05da
aokutu <andreaokutu@gmail.com>
2026-02-07 13:54:53 +0300

    methodsingo

--- methodsingo.go
+++ methodsingo.go
@@ -8,15 +8,22 @@ type Student struct {
 }
 
 func main() {
-
 	var std1 Student
 	std1.name = "ANDREW"
-	fmt.Println(std1.details())
-
+	fmt.Println(std1.name)
+	fmt.Println(std1.details())   // value receiver
+	fmt.Println(std1.details2())  // pointer receiver
+	//fmt.Println(std1.name, &std1.name)
 }
 
-//a method  is a function  with  a type  preceeding the  function name as shown  below 
+// Value receiver
 func (std Student) details() string {
-
+	std.name= "KELVIN"
 	return std.name
+}
+
+// Pointer receiver
+func (stdp *Student) details2() string {
+	stdp.name ="KEN"
+	return stdp.name
 }
