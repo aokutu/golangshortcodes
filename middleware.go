b37a040253ae97ec15b4027207bb805365db5848
aokutu <muleli.haddassah@gmail.com>
2026-05-10 14:03:08 +0300

    basic middleware

---
+++ middleware.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func middleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello World")
+		next(w, r)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Home Page"))
+}
+
+func main() {
+	http.HandleFunc("/", middleware(home))
+
+	fmt.Println("Server running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
