a772a2fd473541c0aa27fc0790ec52c22b393ac5
aokutu <muleli.haddassah@gmail.com>
2026-05-10 14:06:04 +0300

    middleware

--- middleware.go
+++ middleware.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// midddleware
 func middleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Hello World")
